7267323f7ed6125e1eee220bcd981312f1235e05
Corvin74 <moiseev-kn@mail.ru>
2021-03-11 13:14:47 +0300

    lesson6

--- backup_base.sh
+++ backup_base.sh
@@ -4,4 +4,5 @@ DATE=`date +%Y-%m-%d_%H-%M`
 #/opt/1C/v8.3/x86_64/./1cv8  CONFIG /S"xp962_1\demo" /N"admin" /P"12345" /Out"/home/user/1c.log" /DumpIB"/home/user/buh_"$DATE".dt"
 # если без пароля
 /opt/1C/v8.3/x86_64/./1cv8  CONFIG /F /home/support/1C_Base/1C_Learning /Out"/home/support/1C_Base/1C_Learning/backup/1C_Learning_"$DATE".log" /DumpIB"/home/support/1C_Base/1C_Learning/backup/1C_Learning_"$DATE".dt"
-
+/opt/1C/v8.3/x86_64/./1cv8  CONFIG /F /home/support/1C_Base/1C_Learning /Out"/home/support/1C_Base/1C_Learning/backup/config/cf/1C_Learning_"$DATE".log" /DumpCfg"/home/support/1C_Base/1C_Learning/backup/config/cf/1C_Learning_"$DATE".cf"
+/opt/1C/v8.3/x86_64/./1cv8  CONFIG /F /home/support/1C_Base/1C_Learning /Out"/home/support/1C_Base/1C_Learning/backup/config/xml/1C_Learning_"$DATE".log" /DumpConfigToFiles"/home/support/1C_Base/1C_Learning/backup/config/xml/1C_Learning_"$DATE
