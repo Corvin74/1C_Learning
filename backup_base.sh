45f79a81e0182c550e4aab2594219babc817af81
Corvin74 <moiseev-kn@mail.ru>
2021-03-12 14:39:15 +0300

    lesson7

--- backup_base.sh
+++ backup_base.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
+
+COMMIT_MESSAGE=$1
 DATE=`date +%Y-%m-%d_%H-%M`
-# если с паролем 12345
-#/opt/1C/v8.3/x86_64/./1cv8  CONFIG /S"xp962_1\demo" /N"admin" /P"12345" /Out"/home/user/1c.log" /DumpIB"/home/user/buh_"$DATE".dt"
-# если без пароля
-/opt/1C/v8.3/x86_64/./1cv8  CONFIG /F /home/support/1C_Base/1C_Learning /Out"/home/support/1C_Base/1C_Learning/backup/1C_Learning_"$DATE".log" /DumpIB"/home/support/1C_Base/1C_Learning/backup/1C_Learning_"$DATE".dt"
-/opt/1C/v8.3/x86_64/./1cv8  CONFIG /F /home/support/1C_Base/1C_Learning /Out"/home/support/1C_Base/1C_Learning/backup/config/cf/1C_Learning_"$DATE".log" /DumpCfg"/home/support/1C_Base/1C_Learning/backup/config/cf/1C_Learning_"$DATE".cf"
-/opt/1C/v8.3/x86_64/./1cv8  CONFIG /F /home/support/1C_Base/1C_Learning /Out"/home/support/1C_Base/1C_Learning/backup/config/xml/1C_Learning_"$DATE".log" /DumpConfigToFiles"/home/support/1C_Base/1C_Learning/backup/config/xml/1C_Learning_"$DATE
+
+BASE_PATH="/home/support/1C_Base/1C_Learning"
+
+BACKUPLOG_BASE="/home/support/1C_Base/1C_Learning/backup/1C_Learning_"$DATE".log"
+BACKUPLOG_CONFIG_CF="/home/support/1C_Base/1C_Learning/backup/config/cf/1C_Learning_"$DATE".log"
+BACKUPLOG_CONFIG_XML="/home/support/1C_Base/1C_Learning/backup/config/xml/1C_Learning_"$DATE".log"
+
+BACKUP_BASE="/home/support/1C_Base/1C_Learning/backup/1C_Learning_"$DATE".dt"
+BACKUPCONFIG_CF="/home/support/1C_Base/1C_Learning/backup/config/cf/1C_Learning_"$DATE".cf"
+BACKUPCONFIG_XML="/home/support/1C_Base/1C_Learning/backup/config/xml/1C_Learning_"$DATE
+
+/opt/1C/v8.3/x86_64/./1cv8  CONFIG /F $BASE_PATH /Out $BACKUPLOG_BASE /DumpIB $BACKUP_BASE
+/opt/1C/v8.3/x86_64/./1cv8  CONFIG /F $BASE_PATH /Out $BACKUPLOG_CONFIG_CF /DumpCfg $BACKUPCONFIG_CF
+/opt/1C/v8.3/x86_64/./1cv8  CONFIG /F $BASE_PATH /Out $BACKUPLOG_CONFIG_XML /DumpConfigToFiles $BACKUPCONFIG_XML
+
+if [[ -n $COMMIT_MESSAGE ]]; then
+  git add .
+  git commit -m "$COMMIT_MESSAGE"
+  git push
+  git status
+  git log
+fi
+
