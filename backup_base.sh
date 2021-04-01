e2e2b2a97e3289fc50ca0a7ca9b0c380cc524077
Konstantin N. Moiseev <moiseev-kn@mail.ru>
2021-04-01 16:31:21 +0300

    lesson8

--- backup_base.sh
+++ backup_base.sh
@@ -3,15 +3,15 @@
 COMMIT_MESSAGE=$1
 DATE=`date +%Y-%m-%d_%H-%M`
 
-BASE_PATH="/home/support/1C_Base/1C_Learning"
+BASE_PATH=`pwd`
 
-BACKUPLOG_BASE="/home/support/1C_Base/1C_Learning/backup/1C_Learning_"$DATE".log"
-BACKUPLOG_CONFIG_CF="/home/support/1C_Base/1C_Learning/backup/config/cf/1C_Learning_"$DATE".log"
-BACKUPLOG_CONFIG_XML="/home/support/1C_Base/1C_Learning/backup/config/xml/1C_Learning_"$DATE".log"
+BACKUPLOG_BASE=$BASE_PATH"/backup/1C_Learning_"$DATE".log"
+BACKUPLOG_CONFIG_CF=$BASE_PATH"/backup/config/cf/1C_Learning_"$DATE".log"
+BACKUPLOG_CONFIG_XML=$BASE_PATH"/backup/config/xml/1C_Learning_"$DATE".log"
 
-BACKUP_BASE="/home/support/1C_Base/1C_Learning/backup/1C_Learning_"$DATE".dt"
-BACKUPCONFIG_CF="/home/support/1C_Base/1C_Learning/backup/config/cf/1C_Learning_"$DATE".cf"
-BACKUPCONFIG_XML="/home/support/1C_Base/1C_Learning/backup/config/xml/1C_Learning_"$DATE
+BACKUP_BASE=$BASE_PATH"/backup/1C_Learning_"$DATE".dt"
+BACKUPCONFIG_CF=$BASE_PATH"/backup/config/cf/1C_Learning_"$DATE".cf"
+BACKUPCONFIG_XML=$BASE_PATH"/backup/config/xml/1C_Learning_"$DATE
 
 /opt/1C/v8.3/x86_64/./1cv8  CONFIG /F $BASE_PATH /Out $BACKUPLOG_BASE /DumpIB $BACKUP_BASE
 /opt/1C/v8.3/x86_64/./1cv8  CONFIG /F $BASE_PATH /Out $BACKUPLOG_CONFIG_CF /DumpCfg $BACKUPCONFIG_CF
